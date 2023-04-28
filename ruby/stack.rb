add2ed31c8d1b4af0f4b8b17c217d59530bc2ced
Jasmin <ssongnari@gmail.com>
2023-04-28 15:44:50 -0400

    solution

--- ruby/stack.rb
+++ ruby/stack.rb
@@ -11,35 +11,47 @@ class Stack
   # add item to top of stack if not full
   # if full, throw error
   def push(item)
+    raise 'Stack is full' if isFull?
+    @stack.push(item)
   end
 
   # remove item from top of stack and return it
   def pop
+    @stack.empty ? nil : @stack.pop
   end
 
   # return item at top of stack without removing it
   def peek
+    @stack.last
   end
 
   # return true if stack is empty, otherwise false
   def isEmpty?
+    size.zero?
   end
 
   # return true if stack is full, otherwise false
   def isFull?
+    size == @limit
   end
 
   # return number of items in stack
   def size
+    @stack.length
   end
 
   # return -1 if item not in stack, otherwise integer representing 
   # how far it is from the top
   def search(target)
+    @stack.each_with_index do |index, idx|
+      return size - index - 1 if item == target
+    end
+    -1
   end
 
   # print contents of stack: do not return the stack itself!
   def print
+    puts @stack.join(' <- ')
   end
 end
 
